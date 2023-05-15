13a57d70698eb0a5e61a6279eb677338a26348d3
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-05-15 17:23:01 +0530

    4 files modified

--- src/components/pages/abouts/founders/Founder.jsx
+++ src/components/pages/abouts/founders/Founder.jsx
@@ -46,13 +46,23 @@ export default function Founder() {
            </div>
            <div className="col-md-9">
              <h3>Mr. Prahlad Godara</h3>
-             <p>Mr. Godara did his B.com, LLB & Diploma in Labour Law from University of Rajasthan, Jaipur. His corporate work experience spans across 3 decades , working with world ‘s number one two-wheeler automobile company M/S Hero MotoCorp Ltd (earlier Hero Honda Motors Ltd). Currently he is associated with Government of Haryana and working as Advisor to Chairman, Hon’ble by Chief Minister, Haryana.</p>
+             <p>Mr. Godara is B. Com & Law Graduate from University of Rajasthan, Jaipur. </p>
+             <p>He has worked over 3 decades with World 's number 1 two-wheeler company M/S Hero MotoCorp Ltd (formerly known as Hero Honda Motors Ltd) & retired as a Senior General Manager - Human Resource in Feb, 2020</p>
+
+             <p>Post retirement he is working with Sh. Dushyant Chautala, Hon'ble Dy C M, Haryana Government as an Advisor to Chairman, Haryana Building & Construction Worker Welfare Board, Labour Dept
+             He is very passionate to understand social engineering, social set </p>
+             <p>up of our country & wish to work very closely with human beings.</p>
            </div>
          </div>
          <div className="row mt-3 advisor_row">
            <div className="col-md-9">
              <h3>Mr. Vijay Sharma</h3>
-             <p>Mr. Godara did his B.com, LLB & Diploma in Labour Law from University of Rajasthan, Jaipur. His corporate work experience spans across 3 decades , working with world ‘s number one two-wheeler automobile company M/S Hero MotoCorp Ltd (earlier Hero Honda Motors Ltd). Currently he is associated with Government of Haryana and working as Advisor to Chairman, Hon’ble by Chief Minister, Haryana.</p>
+             <h6>Mr. Vijay Sharma  serves as a Director of the Jindal Stainless. </h6>
+             <p>
+              With over 35 years of experience, Mr. Sharma has steered diverse roles in the areas of Corporate affairs, Sales, Distribution, Corporate Branding, Marketing & Business Development, Strategy Formulation, Supply Chain Management, Market research & Product development, Customer outreach & satisfaction in Stainless Steel & Automotive Industries. He also has been overseeing profitability of the Service center chains of the Jindal Stainless Group in India and in Spain.
+              </p>
+              <p>An able orator and an avid social media proponent, Mr. Sharma is a regular presenter in premium institutes, national & international webinars & conferences. His articles/ views on business situations are regularly published in various Industry magazines. He has been awarded multiple accolades for his contribution in Stainless Steel & Automotive Industry.</p>
+              <p>He is also on the Board of companies like Indian Iron & Steel Skill Sector council, Shalimar paints, JSL Lifestyle. He is the Chairman – Haryana state Development Council of ASSOCHAM, Co Chair- Haryana State Chapter PHDCCI. He is member & actively participating in various initiatives of Apex Associations like FICCI, CII, ISA & ISSDA.</p>
            </div>
            <div className="col-md-3">
              <img src="https://www.navvkhushbharat.com/wp-content/uploads/2023/02/WhatsApp-Image-2023-02-13-at-11.50.52-AM.jpeg" class="card-img-top w-100" alt=""/>
@@ -64,13 +74,22 @@ export default function Founder() {
            </div>
            <div className="col-md-9">
              <h3>Sandeep Bhargava</h3>
-             <p>Mr. Godara did his B.com, LLB & Diploma in Labour Law from University of Rajasthan, Jaipur. His corporate work experience spans across 3 decades , working with world ‘s number one two-wheeler automobile company M/S Hero MotoCorp Ltd (earlier Hero Honda Motors Ltd). Currently he is associated with Government of Haryana and working as Advisor to Chairman, Hon’ble by Chief Minister, Haryana.</p>
+             <p>Sandeep Bhargava - BE, MBA, With more than 40 years of National and International Experience in Corporate Affairs and Public Policy, Government Affairs, Enterprise Sales and Program Management. Major Companies worked in India and Europe:  Vodafone Idea Limited  Microsoft Corp  Nokia India  Nokia Siemens Networks  Hughes India Currently Senior Advisor at Telecom and Digital Health Companies and Investor & Mentor at Startups. Social Sector: Member Advisory Board of Sarthak since its inception and Vice President-National Abylimpics Association of India Advisory Board Member, Aambra Foundation Member Advisory Board at Samaagra Foundation.
+</p>
            </div>
          </div>
          <div className="row mt-3 advisor_row">
            <div className="col-md-9">
              <h3>PARUL KUMAR</h3>
-             <p>Mr. Godara did his B.com, LLB & Diploma in Labour Law from University of Rajasthan, Jaipur. His corporate work experience spans across 3 decades , working with world ‘s number one two-wheeler automobile company M/S Hero MotoCorp Ltd (earlier Hero Honda Motors Ltd). Currently he is associated with Government of Haryana and working as Advisor to Chairman, Hon’ble by Chief Minister, Haryana.</p>
+             <p>“Happy people are truly grateful people. They focus on the best things that happen to them every single day. They see a miracle in the smallest aspects of life.”</p>
+
+             <p>PARUL KUMAR, post graduate from LADY IRWIN COLLEGE, University of DELHI and holding a Management degree, has a multi-faceted personality having interest in Theatre and a passion for performing arts. She was actively involved in SPIC MACAY, where she was the National Treasurer. She organized more than 200 classical concerts per</p>
+
+             <p>year. She is a Kathak Dancer too. She has been part of many panel discussions on Youth Forum during college days on social issues. For past fifteen years she has worked intensively as a Social Entrepreneur, building inclusive and participatory society. It’s been a challenging yet satisfactory life</p>
+             
+             <p>She is the Co-founder of PRABHAAV FOUNDATION. This foundation is contributing in field of conscious living with focus on Environment, Education, Health and Culture through various CSR projects.</p>
+
+             <p>In year 2021, She became the CEO of AK INSTITUTE OF OPHTHALMOLOGY, a state of an art Eye Hospital headed by Padma Shree Dr Atul Kumar.</p>
            </div>
            <div className="col-md-3">
              <img src="https://www.navvkhushbharat.com/wp-content/uploads/2023/01/Image-2022-09-19-at-15.26.56.jpg" class="card-img-top w-100" alt=""/>
