8810ff06b86122bc39ed9dae524993f76cdbd71d
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-05-20 11:36:55 +0530

    4 files modified

--- src/components/pages/home/heroButton/SocialSIcon.jsx
+++ src/components/pages/home/heroButton/SocialSIcon.jsx
@@ -6,10 +6,20 @@ export default function SocialsIcons() {
      <>
          <div class="">
             <div className="social_link">
-                <a href=""><i class="fa-brands fa-facebook"></i></a>
+                {/* <a href=""><i class="fa-brands fa-facebook"></i></a>
                 <a href=""><i class="fa-brands fa-twitter"></i></a>
                 <a href=""><i class="fa-brands fa-instagram"></i></a>
-                <a href=""><i class="fa-brands fa-youtube"></i></a>
+                <a href=""><i class="fa-brands fa-youtube"></i></a> */}
+                <img src="http://localhost:3000/image/social-media-icons/icons8-linkedin.svg" alt=""/>
+                <img src="http://localhost:3000/image/social-media-icons/icons8-twitter-squared-48.png" alt=""/>
+                
+                <img src="http://localhost:3000/image/social-media-icons/youtb.svg" class="youtube-icons" alt=""/>
+
+                <img src="http://localhost:3000/image/social-media-icons/icons8-facebook.svg" alt=""/>
+                <img src="http://localhost:3000/image/social-media-icons/icons8-instagram.svg" alt=""/>
+
+
+                
               </div>
          </div>
      </>
