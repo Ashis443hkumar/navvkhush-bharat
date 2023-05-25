577242788a621b1cf3eb768deaa33f0ed4d8490d
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-05-25 13:35:05 +0530

    1 file modified

--- src/components/pages/home/hero/Hero.jsx
+++ src/components/pages/home/hero/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
      <>
       <div className="hero_section">
       <figure>
-         <img class="" src="https://media.licdn.com/dms/image/D4D22AQE9MyzV05eNMg/feedshare-shrink_800/0/1685000889617?e=1687996800&v=beta&t=tOZm1XWF3USvtX67nCmrruQZtP9SDGThasg8F8D9XuU" alt="" />
+         <img class="" src="https://media.licdn.com/dms/image/D4D22AQHMmkTcaAvfuQ/feedshare-shrink_2048_1536/0/1685000871194?e=1687996800&v=beta&t=S4tDQRgqqOnvNSxjN2v9ddrkTE77pwoVh9f_OGLeN4o" alt="" />
        </figure>
       </div>
      </>
