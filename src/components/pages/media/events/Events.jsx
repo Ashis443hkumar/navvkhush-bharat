a5acc7f5e0766c75c3beb2862b45ccb8cf4e568b
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-05-30 14:55:25 +0530

    2 files modified

--- src/components/pages/media/events/Events.jsx
+++ src/components/pages/media/events/Events.jsx
@@ -1,86 +1,12 @@
 import React,{useState} from "react";
-import Heading from "../../../common/heading/Heading";
-import AboutHeroBack from "../../home/hero/AboutHeroback";
 
 
 export default function Events() {
 
   return (
      <>
-
-      <section class="mt-4">
-         <Heading title="Events" />
-         <div className="container mt-3">
-           <div className="row media_content">
-             <div className="col-md-6">
-               <div className="card">
-                 <img src="https://www.seedsindia.org/wp-content/uploads/2023/04/Wazirabad-Lake-1536x1024.jpg" alt="" class="w-100" />
-                 <div className="card-body">
-                  <p>SEEDS restores Wazirabad lake in Gurugram through nature-based solutions</p>
-                 </div>
-               </div>
-             </div>
-             <div className="col-md-6">
-             <div className="card">
-                 <img src="https://media.licdn.com/dms/image/D4D22AQHQd9uGBPVqfA/feedshare-shrink_800/0/1685355472628?e=1687996800&v=beta&t=HHQef6nKpubll1QV5ZHmPPguMe2WAVmWywSyya-e_VE" alt="" class="w-100" />
-                 <div className="card-body">
-                  <p>SEEDS restores Wazirabad lake in Gurugram through nature-based solutions</p>
-                 </div>
-               </div>
-             </div>
-             <div className="col-md-6">
-             <div className="card">
-                 <img src="https://www.navvkhushbharat.com/wp-content/uploads/elementor/thumbs/logo-pzmronjrgqdx9tci7wd8um1iysogmgenu7xx5cd57y.png" alt="" class="w-100" />
-                 <div className="card-body">
-                  <p>navvkhush bharat restores Wazirabad lake in Gurugram through nature-based solutions</p>
-                 </div>
-               </div>
-             </div>
-             <div className="col-md-6">
-             <div className="card">
-                 <img src="https://media.licdn.com/dms/image/D4D22AQEzuZhI7v14LA/feedshare-shrink_800/0/1685347814068?e=1687996800&v=beta&t=KRU-rnTzny0v36_QSutfG29OwnlX78q0pVGNwlEeFfc" alt="" class="w-100" />
-                 <div className="card-body">
-                  <p>navvkhush bharat restores Wazirabad lake in Gurugram through nature-based solutions</p>
-                 </div>
-               </div>
-             </div>
-             <div className="col-md-6">
-             <div className="card">
-                 <img src="https://media.licdn.com/dms/image/D4D22AQHFXLl5DSwEkg/feedshare-shrink_1280/0/1685355472112?e=1687996800&v=beta&t=Z9JlTuzNoSHz7I64NA0Bu_PRXxStzzKRaZhWn1aZ8Sg" alt="" class="w-100" />
-                 <div className="card-body">
-                  <p>navvkhush bharat restores Wazirabad lake in Gurugram through nature-based solutions</p>
-                 </div>
-               </div>
-             </div>
-             <div className="col-md-6">
-             <div className="card">
-                 <img src="https://media.licdn.com/dms/image/D4D22AQE8jVpBjy8UVA/feedshare-shrink_1280/0/1685355471986?e=1687996800&v=beta&t=izNihEchxCsY-rjbbEsd-xhnszrcGA-jvBcGgX1DEY4" alt="" class="w-100" />
-                 <div className="card-body">
-                  <p>navvkhush bharat restores Wazirabad lake in Gurugram through nature-based solutions</p>
-                 </div>
-               </div>
-             </div>
-             <div className="col-md-6">
-             <div className="card">
-                 <img src="https://media.licdn.com/dms/image/D4D22AQH1xunuqkoghA/feedshare-shrink_800/0/1685355472111?e=1687996800&v=beta&t=_fUd4GL3kwkZuNOGLNugLGVSsDvrtzqNFBDNVLZNmDY" alt="" class="w-100" />
-                 <div className="card-body">
-                  <p>navvkhush bharat restores Wazirabad lake in Gurugram through nature-based solutions</p>
-                 </div>
-               </div>
-             </div>
-             <div className="col-md-6">
-             <div className="card">
-                 <img src="https://media.licdn.com/dms/image/D4D22AQEaerTyOPR2WQ/feedshare-shrink_1280/0/1685355472442?e=1687996800&v=beta&t=ZzkgnzBupNozrKxsVl7DP2f59wvqY54DdvHWKj-9-QE" alt="" class="w-100" />
-                 <div className="card-body">
-                  <p>navvkhush bharat restores Wazirabad lake in Gurugram through nature-based solutions</p>
-                 </div>
-               </div>
-             </div>
-
-
-           </div>
-         </div>
-       </section>
+      <h1>event pages</h1>
+   
 
      </>
   );
