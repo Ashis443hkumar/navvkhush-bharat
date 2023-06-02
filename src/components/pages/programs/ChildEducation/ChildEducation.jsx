a0a942fc5ea569837cf4ea00207450b4be389c0a
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-06-02 17:14:00 +0530

    3 files modified

--- src/components/pages/programs/ChildEducation/ChildEducation.jsx
+++ src/components/pages/programs/ChildEducation/ChildEducation.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Heading from "../../../common/heading/Heading";
 import AboutHeroBack from "../../home/hero/AboutHeroback";
-import "./style.css"
+import "./style.css";
+
+// import img1 from "../../../../../images/programImg/digital1.jpeg"
 
 export default function ChildEducation() {
   return (
@@ -10,38 +12,57 @@ export default function ChildEducation() {
      <AboutHeroBack AboutTitle="" cover={"https://media.licdn.com/dms/image/D4D22AQEWndiM_gUeYQ/feedshare-shrink_2048_1536/0/1685533736414?e=1688601600&v=beta&t=dgK-8Hm-76-xQTaB3KU365DEK1X12qLaBJftpMzplIA"} />
        
        {/* <Heading title="CHILD EDUCATION" /> */}
-
        <section class="CHILD_EDUCATION mt-4">
          <div className="container">
-           <div className="row  child_content">
-             <div className="col-12">
-               <h3 class="mt-3">DIGITAL LITERACY</h3>
-               <p>In 2022, we launched a digital literacy awareness programme in collaboration with the Delhi government. The programme focused on creating awareness amongst students about digitalisation and the impact it can have in a post-Covid world. For this programme, we focused on children between the ages of 14 and 18 studying in classes 8th-12th grade. As these children will soon have to prepare for their entrance exams, Covid has significantly impacted their education. We wanted to help these students by teaching them how the internet can help them academically. This is how our team was able to reach out to over 1.5 lakhs of students in government schools. It was a 3-month programme where we interacted frequently with the students to teach them the benefits of the internet.</p>
-
-               <p>During this programme, we taught them how to use browsers such as Google Chrome to find materials that could be useful for their studies. The students were also taught how to create presentations, videos and emails, which should help them prepare for the real world. Since most of these children come from underprivileged backgrounds, we wanted them to excel in this competitive world and be prepared for better jobs by teaching them these skills. So we partnered with 137 schools that share the same vision of helping these children succeed. We also wanted them to </p>
-               
-               <p>see their education beyond the classroom, because the internet is a great way to learn new skills. For example, many educators post their study material and lectures on YouTube, which can help students prepare for their exams. Our goal was to show them how the internet can help them become self-reliant.</p>
-             </div>
+           <div className="row  child_contentt">
+             <h3 class="my-3 pb-4 text-center">DIGITAL LITERACY</h3>
+            <div className="col-lg-4">
+               <img className="w-100" src="https://media.licdn.com/dms/image/D4D22AQEhrzyrFs7h2A/feedshare-shrink_2048_1536/0/1685684251236?e=1688601600&v=beta&t=9Ab2cfoLaAm73oWxpYQNn5xKJGKVqvl01MR_tp0roR4" alt="" />
+            </div>
+            <div className="col-lg-4">
+               <p>During this programme, we taught them how to use browsers such as Google Chrome to find materials that could be useful for their studies. The students were also taught how to create presentations, videos and emails, which should help them prepare for the 
+                real world. Since most of these children come from underprivileged backgrounds, we wanted them to excel in this competitive world and be
+                 prepared for better jobs by teaching them these skills. So we partnered with 137 schools that share the same vision of helping these children
+               </p>
+            </div>
+            <div className="col-lg-4">
+               <img class="w-100" src="https://media.licdn.com/dms/image/D4D22AQFVJN16Qov_mQ/feedshare-shrink_800/0/1685684251181?e=1688601600&v=beta&t=5mxG4a-CYn8KYUUM2xin0RCHko5cejrwx37nvQSDJbk" alt="" />
+            </div>
+            <div className="col-lg-12 child_content">
+              <p> succeed. We also wanted them tosee their education beyond the classroom, because the internet is a great way to learn new skills. For example, many educators post their study material and lectures on YouTube, which can help students prepare for their exams. Our goal was to show them how the internet can help them become self-reliant.</p>
+            </div>
            </div>
 
 
             {/* Navv Khush Bharat School  */}
             <div className="row child_content">
-             <div className="col-md-12">
-               <h3 class="mt-3">Summer Camp</h3>
-               <p>In 2022, we launched a digital literacy awareness programme in collaboration with the Delhi government. The programme focused on creating awareness amongst students about digitalisation and the impact it can have in a post-Covid world. For this programme, we focused on children between the ages of 14 and 18 studying in classes 8th-12th grade. As these children will soon have to prepare for their entrance exams, Covid has significantly impacted their education. We wanted to help these students by teaching them how the internet can help them academically. This is how our team was able to reach out to over 1.5 lakhs of students in government schools. It was a 3-month programme where we interacted frequently with the students to teach them the benefits of the internet.</p>
-
-               <p>During this programme, we taught them how to use browsers such as Google Chrome to find materials that could be useful for their studies. The students were also taught how to create presentations, videos and emails, which should help them prepare for the real world. Since most of these children come from underprivileged backgrounds, we wanted them to excel in this competitive world and be prepared for better jobs by teaching them these skills. So we partnered with 137 schools that share the same vision of helping these children succeed. We also wanted them to </p>
-               
-               <p>see their education beyond the classroom, because the internet is a great way to learn new skills. For example, many educators post their study material and lectures on YouTube, which can help students prepare for their exams. Our goal was to show them how the internet can help them become self-reliant.</p>
+              <h3 class="my-4 pb-3 text-center">Summer Camp</h3>
+              <div className="col-lg-6 summer_camp_img">
+                <img className="" src="https://media.licdn.com/dms/image/D4D22AQEU3DgPR6cdtA/feedshare-shrink_1280/0/1685684251209?e=1688601600&v=beta&t=poqxMjSiDIUtwlkGjX_1rznD_vSBa2ijwHnHKeDeGuk" alt="" />
+              </div>
+             <div className="col-md-6">
+               <p>In 2022, we launched a digital literacy awareness programme in collaboration with the Delhi government. The programme focused on creating awareness amongst students about digitalisation and the impact it can have in a post-Covid world. For this programme, we focused on children between the ages of 14 and 18 studying in classes 8th-12th grade. As these children will soon have to prepare for their entrance exams, Covid has significantly impacted their education. We wanted to help these students by teaching them how the internet can help them academically. This is how our team was able to reach out to over 1.5 lakhs of students in government schools. It was a 3-month programme where we interacted frequently with 
+                the students to teach them the benefits of the internet.</p>
+             </div>
+             <div className="col-md-12 summer_content">
+             <p>During this programme, we taught them how to use browsers such as Google Chrome to find materials that could be useful for their studies. The students were also taught how to create presentations, videos and emails, which should help them prepare for the real world. Since most of these children come from underprivileged backgrounds, we wanted them to excel in this competitive world and be prepared for better jobs by teaching them these skills. So we partnered with 137 schools that share the same vision of helping these children succeed. We also wanted them to </p>
+             <p>see their education beyond the classroom, because the internet is a great way to learn new skills. For example, many educators post their study material and lectures on YouTube, which can help students prepare for their exams. Our goal was to show them how the internet can help them become self-reliant.</p>
+             
              </div>
            </div>
 
 
            {/* Navv Khush Bharat School  */}
-           <div className="row mt-1 child_content">
+           <div className="row mt-4 child_content">
+             <h3 className="text-center mb-3">NavvKhush helping foundation</h3>
+              <div className="col-lg-6">
+                  <img class="" src="https://media.licdn.com/dms/image/D4D22AQHRXtEn5pBZpg/feedshare-shrink_1280/0/1685684249868?e=1688601600&v=beta&t=-7e-oBFp2ubl9jAWutSHfCvIChcY94Ok2ljCYpc-l-k" alt="" />
+              </div>
+              <div className="col-lg-6">
+                <img class="" src="https://media.licdn.com/dms/image/D4D22AQG769umklDoGQ/feedshare-shrink_1280/0/1685684250599?e=1688601600&v=beta&t=TC5ZDv-HiJuzRRKhZjfHeRqp3b3HxNXsKqsl6BDtbww" alt="" />
+              </div>
+             <div className="row mt-2">
              <div className="col-md-12">
-               <h3>NavvKhush Bharat helth founden</h3>
                
                <p>The ultimate aim of this project is to empower disadvantaged children by providing them with the necessary resources and support to break the cycle of poverty. By enrolling these children in school and providing them with everything they need, from clothing and school materials to tutoring and medical facilities, we aim to give them a dignified and happy childhood. </p>
 
@@ -54,14 +75,9 @@ export default function ChildEducation() {
                <p>Our efforts do not end with our own school. We aim to send each and every child to good schools so that they can take advantage of opportunities that would otherwise be out of reach. We also plan to incorporate technology by providing projectors for story time, fun time, and learning.</p>
                
                <p>We invite you to join us on this project and make a difference in the lives of these children. You can visit us anytime and stay connected with us through our social media handles. Together, we can create a brighter future for these children and help break the cycle of poverty.</p>
-
-            
+             </div>
              </div>
            </div>
-
-
-
-
 
          </div>
 
